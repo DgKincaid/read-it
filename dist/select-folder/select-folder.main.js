52fa894830711e907ca20f4df04b6e4479069c23
Douglas Kincaid <DgKincaid@users.noreply.github.com>
2018-07-22 17:09:10 -0400

    move to electron folder

--- dist/select-folder/select-folder.main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class SelectFolderMain {
-    static init() {
-        console.log('Select folder main');
-    }
-}
-exports.SelectFolderMain = SelectFolderMain;
-//# sourceMappingURL=select-folder.main.js.map
--- dist/select-folder/select-folder.renderer.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fileManagerBtn = document.getElementById('open-file-manager');
-fileManagerBtn.addEventListener('click', () => {
-    // shell.showItemInFolder(os.homedir());
-});
-//# sourceMappingURL=select-folder.renderer.js.map
