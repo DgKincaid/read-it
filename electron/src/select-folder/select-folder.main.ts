65d4dfef1590cacca36920a173bc7cbd2dca6d08
Douglas Kincaid <DgKincaid@users.noreply.github.com>
2018-07-23 19:43:17 -0400

    removed electron client

--- electron/src/select-folder/select-folder.main.ts
+++
@@ -1,8 +0,0 @@
-import { ipcMain, IpcMain } from 'electron';
-
-export class SelectFolderMain {
-    
-    public static init(): void {
-        console.log('Select folder main')
-    }
-}
--- electron/src/select-folder/select-folder.renderer.ts
+++
@@ -1,8 +0,0 @@
-import { shell } from 'electron';
-import * as os from 'os';
-
-const fileManagerBtn = document.getElementById('open-file-manager')!;
-
-fileManagerBtn.addEventListener('click', () => {
-    // shell.showItemInFolder(os.homedir());
-})
